72fd2670c6f74d4f6becabc3a6ac5e785b33dcf2
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-09-14 21:49:13 +0530

    avicii: Disable NFC For now

--- device.mk
+++ device.mk
@@ -186,14 +186,14 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
 # NFC
-PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0:64 \
-    android.hardware.nfc@1.1:64 \
-    android.hardware.nfc@1.2:64 \
-    android.hardware.secure_element@1.0:64 \
-    com.android.nfc_extras \
-    NfcNci \
-    Tag \
+#PRODUCT_PACKAGES += \
+#    android.hardware.nfc@1.0:64 \
+#    android.hardware.nfc@1.1:64 \
+#    android.hardware.nfc@1.2:64 \
+#    android.hardware.secure_element@1.0:64 \
+#    com.android.nfc_extras \
+#    NfcNci \
+#    Tag \
 
 # Power
 PRODUCT_PACKAGES += \
